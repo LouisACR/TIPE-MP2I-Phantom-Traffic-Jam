ad6b3c692be33d6441e9f5e6b01586be144db1f7
Clement Prevot <clement.prevot@numericable.fr>
2022-05-11 14:52:10 +0200

    test moi qui fait javascipt

--- main.js
+++ main.js
@@ -9,22 +9,10 @@ var params = {
   var radius = 50;
   var x = two.width * 0.5;
   var y = two.height * 0.5 - radius * 1.25;
-  var circle = two.makeCircle(x, y, radius);
-  
-  y = two.height * 0.5 + radius * 1.25;
-  var width = 100;
-  var height = 100;
-  var rect = two.makeRectangle(x, y, width, height);
-  
-  // The object returned has many stylable properties:
-  circle.fill = '#FF8000';
-  // And accepts all valid CSS color:
-  circle.stroke = 'orangered';
-  circle.linewidth = 5;
-  
-  rect.fill = 'rgb(0, 200, 255)';
-  rect.opacity = 0.75;
-  rect.noStroke();
+
+  var voit = two.makeRectangle(x,y,100,100);
+
+  voit.fill="#32a852";
   
   // Don’t forget to tell two to draw everything to the screen
   two.update();
