525be3380e593c4f63207c9a630a0864e5cdaeeb
Striladen <striladen@gmail.com>
2022-05-11 15:55:13 +0200

    couleurs

--- main.js
+++ main.js
@@ -12,18 +12,26 @@ var height = two.height;
 var x = width * 0.5;
 var y = height * 0.5 - radius * 1.25;
 
-var voit = two.makeRectangle(x,y,100,100);
+var voit = two.makeRectangle(x,y,50,50);
 
 voit.fill="#32a852";
 
 let rectangles = [];
+let red = 0;
+let green = 0;
+let blue = 0;
 
 var mouseY = 0;
 var mouseX = 0;
 
 document.addEventListener("click", () => {
   rectangles.push([mouseX, mouseY]);
-  two.makeRectangle(mouseX,mouseY,100, 100);
+  var rect = two.makeCircle(mouseX,mouseY,100);
+  red=(red+(Math.random()*10))%255;
+  green=(green+(Math.random()*10))%255;
+  blue=(blue+(Math.random()*10))%255;
+  rect.fill = "rgb("+red+", "+green+", "+blue+")";
+  rect.noStroke();
 });
 
 document.addEventListener("mousemove", () => {
