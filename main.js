8f641e77066827f9538b4b0e93b5507415cdea0c
Striladen <striladen@gmail.com>
2022-05-11 15:47:20 +0200

    click and move

--- main.js
+++ main.js
@@ -1,30 +1,45 @@
 // Make an instance of two and place it on the page.
 var params = {
-    fullscreen: true
-  };
-  var elem = document.getElementById('canvas');
-  var two = new Two(params).appendTo(elem);
-  
-  // Two.js has convenient methods to make shapes and insert them into the scene.
-  var radius = 50;
-  var width = two.width;
-  var height = two.height;
-  var x = width * 0.5;
-  var y = height * 0.5 - radius * 1.25;
+  fullscreen: true
+};
+var elem = document.getElementById('canvas');
+var two = new Two(params).appendTo(elem);
 
-  var voit = two.makeRectangle(x,y,100,100);
+// Two.js has convenient methods to make shapes and insert them into the scene.
+var radius = 50;
+var width = two.width;
+var height = two.height;
+var x = width * 0.5;
+var y = height * 0.5 - radius * 1.25;
 
-  voit.fill="#32a852";
-  
-  // Don’t forget to tell two to draw everything to the screen
-  two.update();
+var voit = two.makeRectangle(x,y,100,100);
 
-  two.bind("update", function (frameCount) {
-    var x = voit.translation.x;
-    var y = voit.translation.y;
-    var offsetX = 5;
-    var offsetY = 0;
-    voit.translation = new Two.Vector((x + offsetX)%width,(y + offsetY)%height);
+voit.fill="#32a852";
+
+let rectangles = [];
+
+var mouseY = 0;
+var mouseX = 0;
+
+document.addEventListener("click", () => {
+  rectangles.push([mouseX, mouseY]);
+  two.makeRectangle(mouseX,mouseY,100, 100);
+});
+
+document.addEventListener("mousemove", () => {
+  mouseY = event.clientY;
+  mouseX = event.clientX;
+});
+
+
+two.update();
+
+two.bind("update", function (frameCount) {
+  var x = mouseX;
+  var y = mouseY;
+  var offsetX = 0;
+  var offsetY = 0;
+  voit.translation = new Two.Vector((x + offsetX)%width,(y + offsetY)%height);
 });
  
 two.play();
