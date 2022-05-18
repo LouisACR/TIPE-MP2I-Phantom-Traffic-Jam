9934e4fcead18f214f427309d0282df2f024a81f
Clement Prevot <clement.prevot@numericable.fr>
2022-05-18 14:43:45 +0200

    add function createRoad

--- main.js
+++ main.js
@@ -7,12 +7,23 @@ var two = new Two(params).appendTo(elem);
 
 var width = two.width;
 var height = two.height;
+var widthRoad = 30;
+var nbRoad = Math.trunc(height / widthRoad);
+
+function createRoad(){
+  for(var i =0 ; i<nbRoad+1 ; i++){
+    var line = two.makeLine(0, i*widthRoad, width, i*widthRoad);
+  }
+  console.log(nbRoad);
+}
 
 
-
-
-
-
+function init(){
+  width = two.width;
+  height = two.height;
+  nbRoad = Math.trunc(height / widthRoad);
+  createRoad();
+}
 
 
 // A chaque frame, cette fonction est appelée.
@@ -27,7 +38,11 @@ function onClick(){
 
 }
 
-document.addEventListener("click", onClick());
+document.addEventListener("click", onClick);
+
+init();
+
+window.addEventListener('resize', init);
 
 two.update();
 
