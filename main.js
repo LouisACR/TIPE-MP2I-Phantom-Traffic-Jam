60a10017a7d873f75921f5ec7b04c61a4415e4f2
Striladen <striladen@gmail.com>
2022-05-31 22:18:37 +0200

    bug vitesse

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ const config = {
         display: true,
         title: {
           display: true,
-          text: 'Value'
+          text: 'Vitesse'
         },
         suggestedMin: 0,
         suggestedMax: 20
